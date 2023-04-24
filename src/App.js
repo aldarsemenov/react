9d6a7fdc94cd1e05cbafaaf6ee43f3b5ffcb1c37
Aldar <aldarsemenov@mail.ru>
2023-04-24 11:16:52 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -2,10 +2,63 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let menu2 = [
+
+    {
+      "name": "Поджарка",
+      "ing": [
+        "potato",
+        "farsh",
+        "onion"
+      ],
+      "cost": 150
+
+    },
+    {
+      "name": "Лагман",
+      "ing": [
+        "makaron",
+        "potato",
+        "beef"
+      ],
+      "cost": 200
+    },
+    {
+      "name": "Пицца",
+      "ing": [
+        "cheese",
+        "chicken",
+        "onion",
+        "testo"
+      ],
+      "cost": 300
+    },
+    {
+      "name": "Овощной салат",
+      "ing": [
+        "tomato",
+        "cucumber"
+      ],
+      "cost": 60
+    },
+    {
+      "name": "Капустные котлеты",
+      "ing": [
+        "cabbage",
+        "onion"
+      ],
+      "cost": 90
+    }
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {menu2.map((item)=>(
+          <Image name={item.name} ing={item.ing} cost={item.cost} />
+        ))}
+       
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
