edac64d7008d56f00268dab58af6406ce113c8ab
Aldar <aldarsemenov@mail.ru>
2023-05-27 11:46:43 +0800

    Toolkit Async Thunk

--- src/pages/MenuPage/index.js
+++ src/pages/MenuPage/index.js
@@ -1,17 +1,29 @@
 import { useOutletContext } from "react-router-dom";
 import Menucard from "../../components/Menucard";
-import menu from "../../fakeData/menu";
+import { useDispatch, useSelector } from "react-redux";
+import { loadMenu } from "../../store/slices/menuSlice";
+import { useEffect } from "react";
 
 
 const Menu = () => {
- 
+  const dispatch = useDispatch();
+  const menu = useSelector(state => state.menu)
 
+  useEffect(() => {
+    dispatch(loadMenu())
+  }, [])
   return (
-    <div className="mx-auto grid  gap-5 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5  container">
-      {menu.map((item) => (
-        <Menucard item={item} />
-      ))}
-    </div>
+    <>
+      {menu.loading == "fulfilled" && (
+        <div className="mx-auto grid  gap-5 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5  container">
+          {menu.items.map((item) => (
+            <Menucard item={item} />
+          ))}
+        </div>
+      )}
+      {menu.loading == "pending" && <div className="flex justify-center items-center h-[70vh]">Загрузка.....</div>}
+      {menu.loading == "reject" && <div className="flex justify-center items-center h-[70vh]">Произошла ошибка при загрузке данных</div>}
+    </>
   );
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./slices/cartSlice";
+import menuSlice from "./slices/menuSlice";
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    menu:menuSlice
   },
 });
